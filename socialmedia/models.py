d212b7d2830300b1a28cf9f1a0ced7b4ebffc9db
alibaghani <baghaniali2006@gmail.com>
2024-03-18 19:40:55 +0330

    Follow model was implemented

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from account.models import User
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
