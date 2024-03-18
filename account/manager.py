b4f446f7d9e12214ca296278367907c7ad23d4b3
alibaghani <baghaniali2006@gmail.com>
2024-03-18 15:42:54 +0330

    create superuser method was implemented

--- account/manager.py
+++ account/manager.py
@@ -12,6 +12,31 @@ class UserManager(BaseUserManager):
         return user
 
 
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,username ,phone_number, password, **extra_fields):
+        if not phone_number:
+            raise ValueError('phone number must be set!')
+        user = self.model(phone_number=phone_number,username=username,**extra_fields )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,username,phone_number, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        user = self.create_user(username,phone_number, password, **extra_fields)
+        user.save(using=self._db)
+        if user.is_superuser:
+            return user
+        else:
+            print('not super user')
+
+
+
 
 
 
