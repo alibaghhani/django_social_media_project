20bdb7ee472496fbd7dba2e919c52af484fc92a8
alibaghani <baghaniali2006@gmail.com>
2024-03-18 19:28:47 +0330

    multiselect choice module was removed

--- account/models.py
+++ account/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
 # Create your models here.
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import AbstractUser
 from .manager import UserManager
-from multiselectfield import MultiSelectField
+
 
 
 class User(AbstractUser):
