d37b367934b282d34efadd2d83c0a68b4847eb4b
alibaghani <baghaniali2006@gmail.com>
2024-03-18 22:00:58 +0330

    signup view was implemented

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import User
 
 # Create your views here.
+class SignupView(CreateView):
+    model = User
+    fields = ['username','password','phone_number','biography','hobbies','gender']
+    template_name = 'signup.html'
+    context_object_name = 'user'
