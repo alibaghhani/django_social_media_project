f40f7429eef2b79f546fc1859f538816f1eaac3e
alibaghani <baghaniali2006@gmail.com>
2024-03-18 21:59:38 +0330

    adding models to admin page

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from socialmedia.models import *
+from .models import *
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Follow)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+
+
+
+
