cf8bea97119c71d49eb45ef99587cd9b58903d18
alibaghani <baghaniali2006@gmail.com>
2024-03-25 00:33:19 +0330

    cleaning the codes

--- account/manager.py
+++ account/manager.py
@@ -1,42 +1,16 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
-from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,username ,phone_number, password, **extra_fields):
+    def create_user(self, username, phone_number, password, **extra_fields):
         if not phone_number:
-            raise ValueError('phone number must be set!')
-        user = self.model(phone_number=phone_number,username=username,**extra_fields )
-        user.set_password(password)
-        user.save(using=self._db)
+            raise ValueError('Phone number must be set!')
+        user = self.model(username=username, phone_number=phone_number, **extra_fields)
+        user.set_password(user.password)
+        user.save()
         return user
 
-
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
-from django.db import models
-
-
-class UserManager(BaseUserManager):
-    def create_user(self,username ,phone_number, password, **extra_fields):
-        if not phone_number:
-            raise ValueError('phone number must be set!')
-        user = self.model(phone_number=phone_number,username=username,**extra_fields )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self,username,phone_number, password, **extra_fields):
+    def create_superuser(self, username, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        user = self.create_user(username,phone_number, password, **extra_fields)
-        user.save(using=self._db)
-        if user.is_superuser:
-            return user
-        else:
-            print('not super user')
-
-
-
-
-
-
+        return self.create_user(username, phone_number, password, **extra_fields)
